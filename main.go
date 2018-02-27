ac66d0e5d50ac11d5d05bc048c835c684bdcd2a6
Arthur Diniz <arthurbdiniz@gmail.com>
2018-02-27 00:17:18 -0300

    Create func to get a deck from file
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("my_cards")
-
-	// hand, remainingDeck := deal(cards, 5)
-
-	// hand.print()
-	// remainingDeck.print()
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
